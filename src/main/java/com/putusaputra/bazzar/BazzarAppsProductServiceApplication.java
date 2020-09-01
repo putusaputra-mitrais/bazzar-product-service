cb484164fd39730810c96455af135c1951ca057e
putusaputra <iputusaputra@gmail.com>
2020-09-01 14:44:15 +0800

    Initial commit

---
+++ src/main/java/com/putusaputra/bazzar/BazzarAppsProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.putusaputra.bazzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazzarAppsProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazzarAppsProductServiceApplication.class, args);
+	}
+
+}
